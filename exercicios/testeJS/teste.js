f0f5e6c62d01f71efd4bdd1195ab823e48fc4b81
gabasxd <gabriel.costa050401@gmail.com>
2023-08-24 23:47:42 -0300

    javascript-code-att

---
+++ exercicios/testeJS/teste.js
@@ -0,0 +1,10 @@
+function adicionaringrediente() {
+    
+    var ing = document.getElementById("ingrediente").value;
+    
+    var listahtml = document.getElementById("lista").innerHTML;
+    
+    listahtml = listahtml + "<li>" + ing + "</li>"
+    
+    document.getElementById("lista").innerHTML = listahtml;
+}
